f7c4e12e57c6eefd4a79585a36bc14d37eb324ea
aizealawin <ktmudalige@gmail.com>
2023-05-04 16:56:42 -0400

    initial commit

---
+++ src/main/java/com/example/whattoplay/WhattoplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.whattoplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattoplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhattoplayApplication.class, args);
+	}
+
+}
