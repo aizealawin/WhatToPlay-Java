13c7ce25723630cc65fbadce981ba7db771781ff
aizealawin <ktmudalige@gmail.com>
2023-05-04 17:09:28 -0400

    initializing video game model

---
+++ src/main/java/com/example/whattoplay/videogame/Videogame.java
@@ -0,0 +1,9 @@
+package com.example.whattoplay.videogame;
+
+public class Videogame {
+    private Long id;
+    private String name;
+    private String poster;
+    private String description;
+    private Integer achievements;
+}
